16a929c94293f4368bc01caee3ce38c91dc6162d
Khalil Triki <khalil.triki@esprit.tn>
2026-04-20 20:04:50 +0100

    auditor feature

--- src/app/dashboards/auditor/dashboard-auditor.component.spec.ts
+++ src/app/dashboards/auditor/dashboard-auditor.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { DashboardAuditorComponent } from './dashboard-auditor.component';
+import { AuditService } from '../../services/audit.service';
 
 describe('DashboardAuditorComponent', () => {
   let component: DashboardAuditorComponent;
@@ -8,7 +11,17 @@ describe('DashboardAuditorComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [DashboardAuditorComponent]
+      declarations: [DashboardAuditorComponent],
+      imports: [RouterTestingModule],
+      providers: [
+        {
+          provide: AuditService,
+          useValue: {
+            getReport: () => of({ total: 0, countsByAction: {}, topEntities: [], raw: null }),
+            getLogs: () => of({ items: [], total: 0, page: 1, limit: 6 })
+          }
+        }
+      ]
     })
     .compileComponents();
 
