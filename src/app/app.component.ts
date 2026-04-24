fcfd927bc362c726b1386305e37a398ddce6d296
YAHYA KAROUI <yahyakaroui@esprit.tn>
2026-04-24 19:57:01 +0100

    supp agent 😞

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { VoiceWakeService } from './services/voice-wake.service';
+import { Component } from '@angular/core';
 import { LanguageService } from './services/i18n/language.service';
 
 @Component({
@@ -7,21 +6,12 @@ import { LanguageService } from './services/i18n/language.service';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent {
   title = 'Frontend_Medifollow';
 
   constructor(
-    private readonly wake: VoiceWakeService,
     private languageService: LanguageService
   ) {
     this.languageService.initialize();
-  }
-
-  ngOnInit(): void {
-    this.wake.startListening();
-  }
-
-  ngOnDestroy(): void {
-    this.wake.stopListening();
   }
 }
